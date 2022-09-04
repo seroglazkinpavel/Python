f49e86e4c77da669f42807b7b45c719b44b12a52
PavelSeroglazlin <mr.seroglazkin@bk.ru>
2022-09-04 16:30:34 +0400

    Установлена папка c файлами: Practice_6

--- Practice_4/task_1.py
+++ Practice_4/task_1.py
@@ -4,7 +4,7 @@ import math
 def prime_factors(num):
     while num % 2 == 0:
         print(2, )
-        num = num / 2
+        num //= 2
 
     for i in range(3, int(math.sqrt(num)) + 1, 2):
         while num % i == 0:
